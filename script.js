c29f38ad711a1eb5d4bf713c2610b81485609d2c
Lorans <elornorthwind@gmail.com>
2020-06-26 23:30:47 +0300

    basic functional and styling done bad

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-console.log('tic tack')
+function displayClock() {
+    let timer = setInterval(clockTimer, 1000);
+    clockTimer();
+}
 
 
-
-
-function setTimer(node) {
-
+function clockTimer() {
+    let node = document.getElementById('timer');
+    let numbers = node.getElementsByTagName("span");
     let now = new Date();
-    let time = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
-    node.textContent = time;
+    let time = pad(now.getHours(), 2) +
+        + pad(now.getMinutes(), 2) +
+        + pad(now.getSeconds(), 2);
+
+    for (let i = 0; i < time.length; i++) {
+        numbers[i].textContent = time[i];
+
+    }
 
 }
+
+function pad(number, figures) {
+    return ('0' + number).slice(-figures)
+}
+
+displayClock();
